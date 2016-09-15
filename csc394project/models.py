d89bcecbc55465602d70b0c52fab051df534339a
ebarns <erik.barns@yahoo.com>
2016-09-15 19:13:36 +0000

    model update

--- csc394project/models.py
+++ csc394project/models.py
@@ -1,16 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-#extend Django User model
-class Users(User):
-    creditCnt  = models.IntegerField()
-    isEnrolled  = models.BooleanField()
-    isFaculty   = models.BooleanField()
 
 #Course Table
 class Courses(models.Model):
     course_id   = models.CharField(max_length=6, unique=True)
-    name        = models.CharField(max_length=128,unique=True)
+    name        = models.CharField(max_length=128)
     description = models.CharField(max_length=128)
     prereq      = models.CharField(max_length=128)
     credits     = models.IntegerField()
@@ -23,17 +18,24 @@ class Courses(models.Model):
 #Degree (Degree ID, Degree Name)    
 class Degrees(models.Model):
     name        = models.CharField(max_length=128, unique=True)
-    credits     = models.IntegerField()
-    
+    reqcredits  = models.IntegerField()
+    online      = models.BooleanField()
 
 #Degree Requirements     
 class DegreeRequirements(models.Model):
     degree_id   = models.ForeignKey(Degrees.id)
     course_id   = models.ForeignKey(Courses.course_id)
-    required    = models.BooleanField()
+    #required    = models.BooleanField()
     phase       = models.IntegerField()
-    online      = models.BooleanField()
     
+#extend Django User model
+class Users(User):
+    degree      = models.ForeignKey(Degrees.id)
+    creditCnt   = models.IntegerField()
+    isEnrolled  = models.BooleanField()
+    isFaculty   = models.BooleanField()    
     
-    
+class CompletedClasses(models.Model):
+    studentID   = models.ForeignKey(Users.id)
+    courseID    = models.ForeignKey(Courses.id)
     
