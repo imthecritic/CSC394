6e3edc60a18ddeb029e9c8188cc457d1970e67a3
ebarns <erik.barns@yahoo.com>
2016-09-19 22:35:47 +0000

    views update

--- csc394project/views.py
+++ csc394project/views.py
@@ -1,5 +1,22 @@
 from django.http import HttpResponse
 
 
+#INDEX PAGE
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+    
+def login(request):
+    pass
+
+def register(request):
+    pass
+
+def coursecatalog(request):
+    pass
+
+def plan(request):
+    pass
+
+def settings(request):
+    pass
+
