40254368ed02c1e844f8b596cdb091e35ae94298
ebarns <erik.barns@yahoo.com>
2016-09-20 22:08:34 +0000

    added base html file for pages header and footer

--- csc394project/settings.py
+++ csc394project/settings.py
@@ -57,6 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
+            './main/templates/',
             ],
         'APP_DIRS': True,
         'OPTIONS': {
--- main/views.py
+++ main/views.py
@@ -5,7 +5,7 @@ from django.template.response import TemplateResponse
 #INDEX PAGE
 def index(request):
    # return HttpResponse('<html lang="en"><head><meta charset="utf-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1">  <!-- Latest compiled and minified CSS --> <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css"> <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script> </head> <body> <h1>Course Planner</h1> <button>Plan</button> </body>')
-    return HttpResponse('We are winning')
+    return render(request, 'main/index.html', {})
 def login(request):
     pass
 
