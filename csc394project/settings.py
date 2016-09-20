6b55df7f3d2e32131ef94b17109e42e10256f32b
ebarns <erik.barns@yahoo.com>
2016-09-20 01:37:05 +0000

    added models and planner to app folder

--- csc394project/settings.py
+++ csc394project/settings.py
@@ -56,7 +56,8 @@ ROOT_URLCONF = 'csc394project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- main/models.py
+++ main/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+#Course Table
+class Courses(models.Model):
+    course_id   = models.CharField(max_length=6, unique=True)
+    name        = models.CharField(max_length=128)
+    description = models.CharField(max_length=128)
+    prereq      = models.CharField(max_length=128)
+    credits     = models.IntegerField()
+    fall        = models.BooleanField()
+    winter      = models.BooleanField()
+    spring      = models.BooleanField()
+    summer      = models.BooleanField()
+    dormant     = models.BooleanField()
+
+#Degree (Degree ID, Degree Name)    
+class Degrees(models.Model):
+    name        = models.CharField(max_length=128, unique=True)
+    reqcredits  = models.IntegerField()
+    online      = models.BooleanField()
+
+#Degree Requirements     
+class DegreeRequirements(models.Model):
+    degree_id   = models.ForeignKey(Degrees.id)
+    course_id   = models.ForeignKey(Courses.course_id)
+    #required    = models.BooleanField()
+    phase       = models.IntegerField()
+    
+#extend Django User model
+class Users(User):
+    degree      = models.ForeignKey(Degrees.id)
+    creditCnt   = models.IntegerField()
+    isEnrolled  = models.BooleanField()
+    isFaculty   = models.BooleanField()    
+    
+class CompletedClasses(models.Model):
+    studentID   = models.ForeignKey(Users.id)
+    courseID    = models.ForeignKey(Courses.id)
+    
