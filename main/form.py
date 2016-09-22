f7ed3c330c3f31bd9b4d15b0ee9d4adadb36ebcc
ebarns <erik.barns@yahoo.com>
2016-09-22 04:59:37 +0000

    account settings page

--- main/form.py
+++ main/form.py
@@ -2,20 +2,29 @@ from django import forms
 from models import Users
 from django.contrib.auth.forms import UserCreationForm
 
-class RegistrationForm(UserCreationForm):
-    stu_id  = forms.CharField(label="Student ID", required=True)
-    email   = forms.EmailField(label="Email", required=True)
+class AccountForm(forms.Form):
     first   = forms.CharField(label="First name",required=True)
     last    = forms.CharField(label="Last name", required=True)
+    usrname = forms.CharField(label="Username", required=True)
+    email   = forms.EmailField(label="Email", required=True)
     mjr     = forms.ChoiceField(label="Major", choices=[(1,'BS CS'),(2,'BS IT'),(3,'MS CS'),(4,'MS IT')],required=True)
-    enrled  = forms.BooleanField(label="Enrolled")
-    fclty   = forms.BooleanField(label="Faculty")
+    enrled  = forms.BooleanField(initial=True, label="Enrolled")
+    fclty   = forms.BooleanField(initial=True, label="Faculty")
+    
+    
+class RegistrationForm(UserCreationForm):
+    first   = forms.CharField(label="First name",required=True)
+    last    = forms.CharField(label="Last name", required=True)
+    email   = forms.EmailField(label="Email", required=True)
+    mjr     = forms.ChoiceField(label="Major", choices=[(1,'BS CS'),(2,'BS IT'),(3,'MS CS'),(4,'MS IT')],required=True)
+    enrled  = forms.BooleanField(initial=True, label="Enrolled")
+    fclty   = forms.BooleanField(initial=True, label="Faculty")
     
         
     def save(self,commit = True):
         user = super(RegistrationForm, self).save(commit=False)
-        user.first  = self.cleaned_data['first']
-        user.last   = self.cleaned_data['last']
+        user.first_name  = self.cleaned_data['first']
+        user.last_name   = self.cleaned_data['last']
         user.email  = self.cleaned_data['email']
         user.degree = self.cleaned_data['mjr']
         
--- main/urls.py
+++ main/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     url(r'^browse',views.coursecatalog, name="browse"),
     url(r'^plan',views.plan, name="plan"),
     url(r'^about',views.about, name="about"),
-    url(r'^settings',views.settings, name="settings"),
+    url(r'^account',views.account, name="account"),
 ]
